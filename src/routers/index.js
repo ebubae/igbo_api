07afb5d72933d503a285022a825c00243d6da11a
Ebube Chuba <ebube97@me.com>
2020-10-19 20:34:33 -0700

    remove everything

--- src/routers/index.js
+++
@@ -1,9 +0,0 @@
-import editRouter from './editRouter';
-import router from './router';
-import testRouter from './testRouter';
-
-export {
-  editRouter,
-  router,
-  testRouter,
-};
--- src/shared/constants/populateDocuments.js
+++
@@ -1,9 +0,0 @@
-const POPULATE_EXAMPLE = {
-  path: 'examples',
-};
-
-const POPULATE_ASSOCIATED_WORDS = {
-  path: 'associatedWords',
-};
-
-export { POPULATE_EXAMPLE, POPULATE_ASSOCIATED_WORDS };
